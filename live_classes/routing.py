122db038c10fd297e06189e9c559d63584e4904f
Rajeev Swami <rajeevswami056@gmail.com>
2026-04-21 18:15:10 +0530

    Delete live_classes directory

--- live_classes/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from .consumers import LiveClassConsumer
-
-websocket_urlpatterns = [
-    re_path(r'^ws/live-class/(?P<live_class_id>[0-9a-f-]+)/$', LiveClassConsumer.as_asgi()),
-]
